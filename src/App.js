61cc853f7f22a18a8488e5056f0632679b0bbe16
Nithya Parambhath <nithyap139@gmail.com>
2023-10-26 18:17:28 +0530

    Background Color Changer App Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [color,setColor] = useState("black")
+
+  const changeBg =(color)=>{
+    setColor(color)
+  }
+
+  useEffect(()=>{
+    document.body.style.backgroundColor = color
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh'}} className='d-flex flex-column align-items-center justify-content-center text-light'>
+      <h1>Background Color Change</h1>
+
+      <div style={{width:'20%'}} className="buttons d-flex align-items-center justify-content-around mt-3">
+          <button onClick={()=>changeBg('red')} className='btn btn-light'>Red</button>
+          <button onClick={()=>changeBg('blue')} className='btn btn-light'>Blue</button>
+          <button onClick={()=>changeBg('green')} className='btn btn-light'>Green</button>
+          <button onClick={()=>changeBg('purple')} className='btn btn-light'>Purple</button>
+      </div>
     </div>
   );
 }
